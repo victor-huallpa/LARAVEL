4e1efe189c858e7526557660296a755a1df50768
victor-huallpa <vech.600@gmail.com>
2025-03-01 18:40:19 -0500

    MARKDOWN LARAVEL, ESTILOS PARA ENVIAR UN CORREO

--- app/Mail/PostCreateMail.php
+++ app/Mail/PostCreateMail.php
@@ -42,7 +42,7 @@ class PostCreateMail extends Mailable
     public function content(): Content
     {
         return new Content(
-            view: 'emails.post-create',
+            markdown: 'emails.post-create',
         );
     }
 
--- resources/views/emails/post-create.blade.php
+++ resources/views/emails/post-create.blade.php
@@ -1,20 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>
-        Correo por abrobar post
-    </h1>
-    
-    <p>
-        Se a creado un nuevo post que necesita ser aprobado
-    </p>
+<x-mail::message>
+# Correo por abrobar post
 
-    <a href="{{route('posts.show', $post)}}">Click para aprobar</a>
-</body>
-</html>
+<x-mail::panel>
+Se a creado un nuevo post que necesita ser aprobado
+</x-mail::panel>
+
+<x-mail::button :url="route('posts.index')" color="success">
+Click para aprobar
+</x-mail::button>
+</x-mail::message>
